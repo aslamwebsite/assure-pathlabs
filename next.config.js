3ebca009e8d9de76f89773f574e7d735b98437ce
aslamwebsite <108674067+aslamwebsite@users.noreply.github.com>
2023-10-12 13:49:58 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: false,
+    distDir:'build',
+    devIndicators: {
+      buildActivity: false,
+    },
+    images: {
+      domains: ['example.com'],
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "example.com",
+          port: "",
+          pathname: "/**",
+        },
+      ],
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    }
+  };
+  
+  module.exports = nextConfig;
