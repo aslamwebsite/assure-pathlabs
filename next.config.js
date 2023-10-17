8aa63b7caedd927fdbf7f503fe15dfcb5e968837
Aslam Abbas <aslam.triverse@gmail.com>
2023-10-17 13:52:25 +0530

    firstcomit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    distDir:'build',
-    devIndicators: {
-      buildActivity: false,
-    },
-  };
-  
-  module.exports = nextConfig;
+  reactStrictMode: false,
+  distDir:'build',
+  devIndicators: {
+    buildActivity: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
+};
+
+module.exports = nextConfig;
